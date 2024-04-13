d95d144b498116b3466868df821ced5eb1fdbe57
Kamil955 <kamil.kozik@microsoft.wsei.edu.pl>
2024-04-13 11:32:29 +0200

    Creating controllers

--- CarDealership/obj/Debug/net8.0/CarDealership.AssemblyInfo.cs
+++ CarDealership/obj/Debug/net8.0/CarDealership.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDealership")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ca85b3b109c2d666672c4f96996c08701a8e2a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDealership")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarDealership")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
